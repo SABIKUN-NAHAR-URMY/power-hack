3ed93530c94f046bd6716bcc6efefcf9b8e909c5
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2023-01-29 16:03:38 +0600

    from modal data get and post in database

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { router } from './components/Router/Router';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>
+      <Toaster />
     </div>
   );
 }
