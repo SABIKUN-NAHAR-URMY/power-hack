adc96202f2eff867b959738023bdcd5b907ae173
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2023-01-30 10:20:06 +0600

    search done

--- src/components/BillingPage/BillingPage.js
+++ src/components/BillingPage/BillingPage.js
@@ -6,12 +6,13 @@ import AllBillingList from '../AllBillingList/AllBillingList';
 const BillingPage = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
     const [allBillingList, setAllBillingList] = useState([]);
+    const [searchItem, setSearchItem] = useState("");
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('http://localhost:5000/billing-list')
-        .then(res => res.json())
-        .then(data => setAllBillingList(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setAllBillingList(data))
+    }, [])
 
     const handelAdd = (data) => {
         console.log(data);
@@ -60,91 +61,98 @@ const BillingPage = () => {
         }
     }
 
-return (
-    <div className='mx-20'>
-        <div className='border bg-slate-400 flex justify-between items-center p-2'>
-            <div className='flex gap-5'>
-                <p className='text-2xl font-bold'>Billings</p>
-                <input type="text" placeholder="Search" className="input input-bordered input-sm w-full max-w-xs" />
-            </div>
+    return (
+        <div className='mx-20'>
+            <div className='border bg-slate-400 flex justify-between items-center p-2'>
+                <div className='flex gap-5'>
+                    <p className='text-2xl font-bold'>Billings</p>
+                    <input type="text" placeholder="Search by FullName or Phone or Email" onChange={(e) => setSearchItem(e.target.value)} className="input input-bordered input-sm w-[300px]" />
+                </div>
 
-            <div>
-                {/* The button to open modal */}
-                <label htmlFor="my-modal-3" className="btn">Add New Bill</label>
+                <div>
+                    {/* The button to open modal */}
+                    <label htmlFor="my-modal-3" className="btn">Add New Bill</label>
 
-                {/* Put this part before </body> tag */}
-                <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-                <div className="modal">
-                    <div className="modal-box relative">
-                        <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                        <form onSubmit={handleSubmit(handelAdd)}>
-                            {/* Full Name */}
-                            <div className="form-control w-full">
-                                <label className="label"><span className="label-text">Full Name</span></label>
-                                <input type="text"
-                                    {...register("fullName",
-                                        { required: "Full Name is required" })} className="input input-bordered w-full" />
-                                {errors.fullName && <p className='text-red-600'>{errors.fullName?.message}</p>}
-                            </div>
-                            {/* Email */}
-                            <div className="form-control w-full">
-                                <label className="label"><span className="label-text">Email</span></label>
-                                <input type="email"
-                                    {...register("email",
-                                        { required: "Email is required" })} className="input input-bordered w-full" />
-                                {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
-                            </div>
-                            {/* Phone */}
-                            <div className="form-control w-full">
-                                <label className="label"><span className="label-text">Phone</span></label>
-                                <input type="text"
-                                    {...register("phone",
-                                        { required: "Phone is required" })} className="input input-bordered w-full" />
-                                {errors.phone && <p className='text-red-600'>{errors.phone?.message}</p>}
-                            </div>
-                            {/* Paid Amount */}
-                            <div className="form-control w-full">
-                                <label className="label"><span className="label-text">Paid Amount</span></label>
-                                <input type="text"
-                                    {...register("paidAmount",
-                                        { required: "Paid Amount is required" })} className="input input-bordered w-full" />
-                                {errors.paidAmount && <p className='text-red-600'>{errors.paidAmount?.message}</p>}
-                            </div>
-                            <input className='btn w-full mt-5' value='Add New Bill' type="submit" />
+                    {/* Put this part before </body> tag */}
+                    <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+                    <div className="modal">
+                        <div className="modal-box relative">
+                            <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                            <form onSubmit={handleSubmit(handelAdd)}>
+                                {/* Full Name */}
+                                <div className="form-control w-full">
+                                    <label className="label"><span className="label-text">Full Name</span></label>
+                                    <input type="text"
+                                        {...register("fullName",
+                                            { required: "Full Name is required" })} className="input input-bordered w-full" />
+                                    {errors.fullName && <p className='text-red-600'>{errors.fullName?.message}</p>}
+                                </div>
+                                {/* Email */}
+                                <div className="form-control w-full">
+                                    <label className="label"><span className="label-text">Email</span></label>
+                                    <input type="email"
+                                        {...register("email",
+                                            { required: "Email is required" })} className="input input-bordered w-full" />
+                                    {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                                </div>
+                                {/* Phone */}
+                                <div className="form-control w-full">
+                                    <label className="label"><span className="label-text">Phone</span></label>
+                                    <input type="text"
+                                        {...register("phone",
+                                            { required: "Phone is required" })} className="input input-bordered w-full" />
+                                    {errors.phone && <p className='text-red-600'>{errors.phone?.message}</p>}
+                                </div>
+                                {/* Paid Amount */}
+                                <div className="form-control w-full">
+                                    <label className="label"><span className="label-text">Paid Amount</span></label>
+                                    <input type="text"
+                                        {...register("paidAmount",
+                                            { required: "Paid Amount is required" })} className="input input-bordered w-full" />
+                                    {errors.paidAmount && <p className='text-red-600'>{errors.paidAmount?.message}</p>}
+                                </div>
+                                <input className='btn w-full mt-5' value='Add New Bill' type="submit" />
 
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div className='mt-5'>
-            <div className="overflow-x-auto">
-                <table className="table table-compact w-full">
-                    <thead>
-                        <tr>
-                            <th>Billing ID</th>
-                            <th>Full Name</th>
-                            <th>Email</th>
-                            <th>Phone</th>
-                            <th>Paid Amount</th>
-                            <th>Date/Time</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            allBillingList.map(billingList => <AllBillingList
-                            key={billingList._id}
-                            billingList={billingList}
-                            handelDelete={handelDelete}></AllBillingList>)
-                        }
-                    </tbody>
-                </table>
+            <div className='mt-5'>
+                <div className="overflow-x-auto">
+                    <table className="table table-compact w-full">
+                        <thead>
+                            <tr>
+                                <th>Billing ID</th>
+                                <th>Full Name</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                                <th>Paid Amount</th>
+                                <th>Date/Time</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                allBillingList.filter(val =>{
+                                    if(searchItem === ''){
+                                        return val;
+                                    }
+                                    else if((val.fullName.toLowerCase().includes(searchItem.toLowerCase())) || (val.phone.includes(searchItem)) || (val.email.includes(searchItem)) ){
+                                        return val;
+                                    }
+                                }).map(billingList => <AllBillingList
+                                    key={billingList._id}
+                                    billingList={billingList}
+                                    handelDelete={handelDelete}></AllBillingList>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
 };
 
 export default BillingPage;
